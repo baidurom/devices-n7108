02eaec74c931345a2f970bce54c588fc45cd5fae
zoujunhua <zoujunhua@baiyi-mobile.com>
2014-12-02 12:05:29 +0800

    CL-65623: fixed the problem about the camera and music icon..
    Change-Id: I80c92c1a941a088989e33fee1c3a3055cfcf5649

--- custom_app.sh
+++ custom_app.sh
@@ -15,7 +15,10 @@ elif [ "$apkBaseName" = "Settings" ];then
 
 elif [ "$apkBaseName" = "HomePro" ];then
 	echo ">>> in custom_app $apkBaseName"
+
 	sed -i 's/className="com.baidu.camera.Camera" packageName="com.baidu.camera"/className="com.sec.android.app.camera.Camera" packageName="com.sec.android.app.camera"/g' $tempSmaliDir/res/xml-xhdpi/default_workspace.xml
+
+        sed -i '/className="com.baiyi_mobile.security.ui.MainSecurityActivity"/a\<favorite className="com.baidu.musicplayer.activity.MainFragmentActivity" packageName="com.baidu.musicplayer" screen="2" spanX="1" spanY="1" x="1" y="4" \/>' $tempSmaliDir/res/xml-xhdpi/default_workspace.xml
      
 fi
 
